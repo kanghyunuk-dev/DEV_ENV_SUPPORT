87e4d91c2f4f1cb218ff31a91e8899b2ba3a6af0
kanghyunuk <hukang.dev@gmail.com>
2026-04-14 15:35:06 +0900

    V0.2 Master(조장 : 홍길동)

--- src/CH00/C01Master.java
+++ src/CH00/C01Master.java
@@ -5,6 +5,6 @@ public class C01Master {
 	public static void main(String[] args) {
 		// V0.0 Master
 		System.out.print("V0.0 Master(조장 : 홍길동)");
-		
+		System.out.print("V0.2 Master(조장 : 홍길동)");
 	}
 }
